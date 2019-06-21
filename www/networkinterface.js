feb7582b6281c5061eebab6f9660d6c66f0413ff
Domenico Grimaldi <domenico@grimaldistudio.com>
2019-06-21 09:31:23 +0200

    commit

--- www/networkinterface.js
+++ www/networkinterface.js
@@ -1,12 +1,17 @@
-var networkinterface = function() {
+var exec = require('cordova/exec');
+
+window.UsageStatistics = {
+    getUsageStatistics: function(interval, success, error) {
+      	console.log("getUsageStatistics() :: " + interval);
+	  	var array = [interval];
+    	exec(success, error, "MyUsageStatsManager", "getUsageStatistics", array);
+    },
+    
+	openPermissionSettings: function(success, error) {
+		console.log("openPermissionSettings() :: ");
+		var array = []; // not needed but seems to throw exceptions on some cases if not included.
+    	exec(success, error, "MyUsageStatsManager", "openPermissionSettings", array);
+	}
 };
 
-var argscheck = require('cordova/argscheck'),
-    exec = require('cordova/exec');
-
-
-networkinterface.showStats = function(successCallback, failureCallback) {
-	cordova.exec( successCallback, failureCallback, 'networkinterface', 'showStats');
-};
-
-module.exports = networkinterface;
+//module.exports = networkinterface;
